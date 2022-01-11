95189468798a425fbbe5781e6ff62c1863aebd5c
M.K.E Firma <85478005+mkefirma@users.noreply.github.com>
2022-01-11 12:15:24 +0100

    master

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,34 @@ import './Style.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            hello
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            hello
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card">
+            hello
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card">
+            hello
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card">
+            hello
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
