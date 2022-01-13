a063b2479bba5bcd410bcf7e2f67ad9ea6f5d8c7
Emmerseren <85479117+Emmerseren@users.noreply.github.com>
2022-01-13 10:31:38 +0100

    lort

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
+import React from "react";
 import info from "../helpers/info.json";
 
 const Info = () => {
-    return (
-        
-        <div className="container">
-
-            <section className="info row">
-            {info.praktisk.map((item, index)=>
-           
-           
-           <div className=" col-md-6" key={info.praktisk[index].id}> 
-            <div className="infoCard">
-                
-            <h3>{info.praktisk[index].title}</h3>
-            <p> {info.praktisk[index].text1}</p>
-            <p> {info.praktisk[index].text2}</p>
-            <p> {info.praktisk[index].text3}</p>
-            <p> {info.praktisk[index].text4}</p>
+  return (
+    <div className="container">
+      <section className="info row">
+        <div className="col-md-4">
+          <div className="sticky">
+            <div className="info_menu">
+              {info.praktisk.map((item, index) => (
+                <a href={"#" + info.praktisk[index].link}>
+                  {" "}
+                  {info.praktisk[index].title}
+                </a>
+              ))}
             </div>
-
-
-            </div>
-            
-            
-            )}
-            </section>
-        
+          </div>
         </div>
-    )
-}
 
-export default Info
+        <div className=" col-md-8">
+          {info.praktisk.map((item, index) => (
+            <div className="infoCard" id={info.praktisk[index].link}>
+              <h3>{info.praktisk[index].title}</h3>
+              <p> {info.praktisk[index].text1}</p>
+              <p> {info.praktisk[index].text2}</p>
+              <p> {info.praktisk[index].text3}</p>
+              <p> {info.praktisk[index].text4}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Info;
