6e2b7e42338e29b6e5ef523dce2cf39196f3148e
M.K.E Firma <85478005+mkefirma@users.noreply.github.com>
2022-02-11 12:03:15 +0100

    MASTER

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,56 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as ReactDOM from "react-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 import navi from "../../helpers/navigation.json";
 
 const Header = () => {
-<<<<<<< Updated upstream
-=======
-      
-    // const [showHeader, setShowHeader] = useState(true);
-    // const [scrollPos, setScrollPos] = useState(0);
+    const prevScrollY = useRef(0);
+  const [goingUp, setGoingUp] = useState(true);
+  const currentScrollY = 0;
+  const breakpoint = 100;
 
-    //     const handleScroll = () => {
-    //         console.log("top", document.body.getBoundingClientRect().top);
-    //         console.log();
-    //         if (document.body.getBoundingClientRect().top < -116) {
-    //             setScrollPos(document.body.getBoundingClientRect().top);
-    //             setShowHeader(document.body.getBoundingClientRect().top > scrollPos);
-    //         }
-    //     };
-    //     useEffect(() => {
-    //         window.addEventListener("scroll", handleScroll);
-    //         return () => {
-    //             window.removeEventListener("scroll", handleScroll);
-    //         }; 
-    //     });
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+    //   if (prevScrollY.current < currentScrollY && goingUp) {
+    //     setGoingUp(false);
+    //   }
+    //   if (prevScrollY.current > currentScrollY && !goingUp) {
+    //     setGoingUp(true);
+    //   }
 
->>>>>>> Stashed changes
+    if (prevScrollY.current < 175) {
+        setGoingUp(true);
+    } else {
+        setGoingUp(false);
+    }
+
+      prevScrollY.current = currentScrollY;
+    //   console.log(goingUp, currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [goingUp]);
     return (
         <>
-            <header>
+            <header className={currentScrollY + goingUp ? '' : 'fixed'}>
                
                 
-                <div className="bottom">
+                <div className="top">
                     <div className="container">
                     <a href="" className="logo">
                             <img src="../img/Scuba-Logo.png" width={100} alt="" />
                             <span className="title">Diving Djursland</span>
-<<<<<<< Updated upstream
-                            </a>
-=======
                         </a>
                     </div>
                 </div>
                 <div className={`bottom`}>
-                    <div className="container">
->>>>>>> Stashed changes
-                        <ul>
+                    <div className="container menu-flex">
+                        <ul className="left-menu">
                             {navi.navigation.map(( item, index ) => 
                                 <li key={ navi.navigation[index].id }>
                                     <NavLink activeClassName="active" to={ navi.navigation[index].link }>{ navi.navigation[index].title }</NavLink>
                                 </li>
                             )}
+                        </ul>
+                        <ul className="right-menu">
+                            <li>
+                                <a href=""></a>
+                                <ul>
+                                    <li><a href="">DANSK</a></li>
+                                </ul>
+                                <NavLink activeClassName="active" to=""><img src="https://img.icons8.com/color/48/000000/denmark-circular.png"/></NavLink>
+                            </li>
+                            <li>
+                                <NavLink activeClassName="active" to=""><img src="https://img.icons8.com/color/48/000000/usa-circular.png"/></NavLink>
+                            </li>
                         </ul>
                     </div>
                 </div>
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Booking = () => {
     return (
-        <>
-        </>
+
+        <div className="container">
+
+        
+      <div className="tour_card">
+          
+          <div className="tour_img"> <img src="../img/IMG_20190726_122436_1.jpg" alt="" /></div>
+           
+           <h3> Sangstrupklit</h3>
+      </div>
+        </div>
     )
 }
 
