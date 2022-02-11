006b5352cdcc0d79bf87e060ffa64fb658d0dcad
M.K.E Firma <85478005+mkefirma@users.noreply.github.com>
2022-02-11 08:56:41 +0100

    master

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,6 +4,28 @@ import { Routes, Route, NavLink } from "react-router-dom";
 import navi from "../../helpers/navigation.json";
 
 const Header = () => {
+<<<<<<< Updated upstream
+=======
+      
+    // const [showHeader, setShowHeader] = useState(true);
+    // const [scrollPos, setScrollPos] = useState(0);
+
+    //     const handleScroll = () => {
+    //         console.log("top", document.body.getBoundingClientRect().top);
+    //         console.log();
+    //         if (document.body.getBoundingClientRect().top < -116) {
+    //             setScrollPos(document.body.getBoundingClientRect().top);
+    //             setShowHeader(document.body.getBoundingClientRect().top > scrollPos);
+    //         }
+    //     };
+    //     useEffect(() => {
+    //         window.addEventListener("scroll", handleScroll);
+    //         return () => {
+    //             window.removeEventListener("scroll", handleScroll);
+    //         }; 
+    //     });
+
+>>>>>>> Stashed changes
     return (
         <>
             <header>
@@ -14,7 +36,15 @@ const Header = () => {
                     <a href="" className="logo">
                             <img src="../img/Scuba-Logo.png" width={100} alt="" />
                             <span className="title">Diving Djursland</span>
+<<<<<<< Updated upstream
                             </a>
+=======
+                        </a>
+                    </div>
+                </div>
+                <div className={`bottom`}>
+                    <div className="container">
+>>>>>>> Stashed changes
                         <ul>
                             {navi.navigation.map(( item, index ) => 
                                 <li key={ navi.navigation[index].id }>
